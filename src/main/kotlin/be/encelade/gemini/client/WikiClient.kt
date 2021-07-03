d6017dbf28460ca0a7497dfa6dd156a181b0828d
benckx <benoit.vleminckx@restore.eu>
2021-07-03 19:46:16 +0200

    clean up

--- src/main/kotlin/be/encelade/gemini/client/WikiClient.kt
+++ src/main/kotlin/be/encelade/gemini/client/WikiClient.kt
@@ -26,7 +26,7 @@ abstract class WikiClient(private val firstPageOnly: Boolean = false) {
 
     fun listRappers(): List<SearchResultEntry> {
         val url = "https://$lang.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=+incategory:$category"
-        val searchResult = fetch(url)
+        val searchResult = search(url)
         val result = mutableListOf<SearchResultEntry>()
         result += searchResult.query.search
 
@@ -34,7 +34,7 @@ abstract class WikiClient(private val firstPageOnly: Boolean = false) {
             var offset = 10
             while (offset <= searchResult.query.searchinfo.totalhits) {
                 val offsetUrl = "$url&sroffset=$offset"
-                result += fetch(offsetUrl).query.search
+                result += search(offsetUrl).query.search
                 offset += 10
             }
         }
@@ -42,7 +42,7 @@ abstract class WikiClient(private val firstPageOnly: Boolean = false) {
         return result
     }
 
-    private fun fetch(url: String): SearchResult {
+    private fun search(url: String): SearchResult {
         val json = Unirest.get(url).asString().body!!
         println("fetching $url")
         return jsonMapper.readValue(json, SearchResult::class.java)
@@ -53,13 +53,7 @@ abstract class WikiClient(private val firstPageOnly: Boolean = false) {
         println("fetching $url")
 
         val document = Jsoup.connect(url).get()
-        val dateTime1 = localize1(document)
-
-        if (dateTime1 == null) {
-            return localize2(document)
-        } else {
-            return dateTime1
-        }
+        return localize1(document) ?: localize2(document)
     }
 
     private fun localize1(document: Document): LocalDate? {
@@ -81,7 +75,6 @@ abstract class WikiClient(private val firstPageOnly: Boolean = false) {
                     }
                 }
                 .firstOrNull()
-
     }
 
 }
